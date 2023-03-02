06c279807497cbea1d31b6fa5cd6cf09b0763350
Wahid Shafique <wahidshafique42@gmail.com>
2023-03-02 16:08:01 -0500

    fix dark mode ssr issue

--- src/components/DarkLightModeDecider.tsx
+++ src/components/DarkLightModeDecider.tsx
@@ -1,29 +1,42 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const visualModes = ["light", "dark"];
+const visualModes = [
+  {
+    name: "dark",
+    icon: "☾",
+  },
+  { name: "light", icon: "✺" },
+];
 
 // sort of a canary element that sets the initial mode of our app
-export default function DarkLightModeDecider() {
-  const isBrowser = () => typeof window !== "undefined";
-  const [currentVisualMode, setCurrentVisualMode] = useState(
-    isBrowser() && window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? visualModes[1]
-      : visualModes[0]
-  );
+function DarkLightModeDecider() {
+  useEffect(() => {
+    setCurrentVisualMode(
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? visualModes[0]
+        : visualModes[1]
+    );
+  }, []);
+  const [currentVisualMode, setCurrentVisualMode] = useState(visualModes[0]);
+  console.log(currentVisualMode);
   return (
     <button
-      data-visual-mode={currentVisualMode}
+      data-visual-mode={currentVisualMode.name}
       onClick={() => {
         setCurrentVisualMode((e) => {
-          const currentIndex = visualModes.indexOf(e);
+          const currentIndex = visualModes.findIndex(
+            (vsItem) => e.name === vsItem.name
+          );
           const visualModeLen = visualModes.length;
           return visualModes[(currentIndex + 1) % visualModeLen];
         });
       }}
       className="primary-btn"
     >
-      <span>toggle theme</span>
+      <span>{currentVisualMode.icon}</span>
     </button>
   );
 }
+
+export default DarkLightModeDecider;
